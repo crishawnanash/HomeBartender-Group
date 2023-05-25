132d0c514ccb62c4063f9d85c67e6b6db16d78cf
Crishawna <crishawna.nash@gmail.com>
2023-05-25 13:27:19 -0500

    got rid of unnecceary lines

--- src/main/java/org/launchcode/HomeBartender/HomeBartenderApplication.java
+++ src/main/java/org/launchcode/HomeBartender/HomeBartenderApplication.java
@@ -1,13 +1,7 @@
 package org.launchcode.HomeBartender;
 
-import org.launchcode.HomeBartender.controllers.RecipeController;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
 @SpringBootApplication
 public class HomeBartenderApplication {
@@ -17,4 +11,3 @@ public class HomeBartenderApplication {
 	}
 
 }
-//model is where I will create the objects
